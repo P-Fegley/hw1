afd332787d5f26047f25b3b2b868e249d9197942
Preston Fegley <fegley.preston@gmail.com>
2025-01-29 22:56:18 +0000

    inserted credits data

--- insert-credits.sql
+++ insert-credits.sql
@@ -1,4 +1,4 @@
-- Populate credits table
+-- Populate credits table
 INSERT INTO credits (
     movie_id,
     actor_id,
@@ -7,6 +7,132 @@ INSERT INTO credits (
     1,
     1,
     "Bruce Wayne"
+);
+INSERT INTO credits (
+    movie_id,
+    actor_id,
+    role
+) VALUES (
+    1,
+    2,
+    "Alfred"
+);
+INSERT INTO credits (
+    movie_id,
+    actor_id,
+    role
+) VALUES (
+    1,
+    3,
+    "Ra's Al Ghul"
+);
+INSERT INTO credits (
+    movie_id,
+    actor_id,
+    role
+) VALUES (
+    1,
+    4,
+    "Rachel Dawes"
+);
+INSERT INTO credits (
+    movie_id,
+    actor_id,
+    role
+) VALUES (
+    1,
+    5,
+    "Commissioner Gordon"
+);
+INSERT INTO credits (
+    movie_id,
+    actor_id,
+    role
+) VALUES (
+    2,
+    1,
+    "Bruce Wayne"
+);
+INSERT INTO credits (
+    movie_id,
+    actor_id,
+    role
+) VALUES (
+    2,
+    6,
+    "Joker"
+);
+INSERT INTO credits (
+    movie_id,
+    actor_id,
+    role
+) VALUES (
+    2,
+    7,
+    "Harvey Dent"
+);
+INSERT INTO credits (
+    movie_id,
+    actor_id,
+    role
+) VALUES (
+    2,
+    2,
+    "Alfred"
+);
+INSERT INTO credits (
+    movie_id,
+    actor_id,
+    role
+) VALUES (
+    2,
+    8,
+    "Rachel Dawes"
+);
+INSERT INTO credits (
+    movie_id,
+    actor_id,
+    role
+) VALUES (
+    3,
+    1,
+    "Bruce Wayne"
+);
+INSERT INTO credits (
+    movie_id,
+    actor_id,
+    role
+) VALUES (
+    3,
+    5,
+    "Commissioner Gordon"
+);
+INSERT INTO credits (
+    movie_id,
+    actor_id,
+    role
+) VALUES (
+    3,
+    9,
+    "Bane"
+);
+INSERT INTO credits (
+    movie_id,
+    actor_id,
+    role
+) VALUES (
+    3,
+    10,
+    "John Blake"
+);
+INSERT INTO credits (
+    movie_id,
+    actor_id,
+    role
+) VALUES (
+    3,
+    11,
+    "Anne Hathaway"
 );
 
 
